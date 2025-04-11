b0cd23a7dc8973ba0b6c34b3ca455c43bf21fb4c
Ari10yA <khamruiaritya.2.3.5.ak@gmail.com>
2025-04-12 00:07:26 +0530

    new diff array created

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
 const animatorContainer = document.querySelector(".animator");
 const executeButton = document.querySelector(".execute");
 
-const diff_Api = new diff_match_patch();
+// const codeSnippet = document.querySelector(".code-snippet-area");
+// const codeSnippet1 = document.querySelector(".code-snippet-area1");
+// const submitButton = document.querySelector(".submit");
+// const diff_Api = new diff_match_patch();
 
-const map = [
-  { value: -1, key: "Goo" },
-  { value: 1, key: "Ba" },
-  { value: 0, key: "d dog" },
+// submitButton.addEventListener("click", () => {
+//   const currText1 = codeSnippet.value.split("\n").join("<~LB~>");
+//   const currText2 = codeSnippet1.value.split("\n").join("<~LB~>");
+
+//   let diff = diff_Api.diff_main(currText1, currText2);
+//   let newDiff = diff_Api.diff_cleanupSemantic(diff);
+//   console.log(newDiff);
+// });
+
+// const map = [
+//   { value: -1, key: "Goo" },
+//   { value: 1, key: "Ba" },
+//   { value: 0, key: "d dog" },
+// ];
+
+const newMap = [
+  {
+    0: 0,
+    1: "I am the very model of a ",
+  },
+  {
+    0: -1,
+    1: "modern Major-General,<~LB~>I've information vegetable, anim",
+  },
+  {
+    0: 1,
+    1: "cartoon individual,<~LB~>My animation's comical, unusu",
+  },
+  {
+    0: 0,
+    1: "al, and ",
+  },
+  {
+    0: -1,
+    1: "miner",
+  },
+  {
+    0: 1,
+    1: "whimsic",
+  },
+  {
+    0: 0,
+    1: "al,<~LB~>I",
+  },
+  {
+    0: -1,
+    1: " know the kings of England, and I quote the fights historical,<~LB~>From Marathon to Waterloo, in order categorical",
+  },
+  {
+    0: 1,
+    1: "'m quite adept at funny gags, comedic theory I have read,<~LB~>From wicked puns and stupid jokes to anvils that drop on your head",
+  },
+  {
+    0: 0,
+    1: ".",
+  },
 ];
 
 let positionToBeLeftShift = 0;
