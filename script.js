2e94056b90aec9b361f6d4124625db05f5fcc810
Ari10yA <khamruiaritya.2.3.5.ak@gmail.com>
2025-04-12 23:33:27 +0530

    working copy

--- script.js
+++ script.js
@@ -1,118 +1,98 @@
 const animatorContainer = document.querySelector(".animator");
 const executeButton = document.querySelector(".execute");
 
-// const codeSnippet = document.querySelector(".code-snippet-area");
-// const codeSnippet1 = document.querySelector(".code-snippet-area1");
-// const submitButton = document.querySelector(".submit");
-// const diff_Api = new diff_match_patch();
+const codeSnippet = document.querySelector(".code-snippet-area");
+const codeSnippet1 = document.querySelector(".code-snippet-area1");
+const submitButton = document.querySelector(".submit");
+const diff_Api = new diff_match_patch();
 
-// submitButton.addEventListener("click", () => {
-//   const currText1 = codeSnippet.value.split("\n").join("<~LB~>");
-//   const currText2 = codeSnippet1.value.split("\n").join("<~LB~>");
+let calculatedDiff;
 
-//   let diff = diff_Api.diff_main(currText1, currText2);
-//   let newDiff = diff_Api.diff_cleanupSemantic(diff);
-//   console.log(newDiff);
-// });
+submitButton.addEventListener("click", () => {
+  const currText1 = codeSnippet.value.split("\n").join("<~LB~>");
+  const currText2 = codeSnippet1.value.split("\n").join("<~LB~>");
 
-// const map = [
-//   { value: -1, key: "Goo" },
-//   { value: 1, key: "Ba" },
-//   { value: 0, key: "d dog" },
-// ];
+  let diff = diff_Api.diff_main(currText1, currText2);
+  diff_Api.diff_cleanupSemantic(diff);
+  console.log(diff_Api.diff_cleanupSemantic(diff));
 
-const newMap = [
-  {
-    0: 0,
-    1: "I am the very model of a ",
-  },
-  {
-    0: -1,
-    1: "modern Major-General,<~LB~>I've information vegetable, anim",
-  },
-  {
-    0: 1,
-    1: "cartoon individual,<~LB~>My animation's comical, unusu",
-  },
-  {
-    0: 0,
-    1: "al, and ",
-  },
-  {
-    0: -1,
-    1: "miner",
-  },
-  {
-    0: 1,
-    1: "whimsic",
-  },
-  {
-    0: 0,
-    1: "al,<~LB~>I",
-  },
-  {
-    0: -1,
-    1: " know the kings of England, and I quote the fights historical,<~LB~>From Marathon to Waterloo, in order categorical",
-  },
-  {
-    0: 1,
-    1: "'m quite adept at funny gags, comedic theory I have read,<~LB~>From wicked puns and stupid jokes to anvils that drop on your head",
-  },
-  {
-    0: 0,
-    1: ".",
-  },
-];
+  calculatedDiff = diff;
+  executeButton.disabled = false;
+});
 
 let positionToBeLeftShift = 0;
-let positionToBeShiftedAfterAnimation = 0;
+let positionToBeLeftShiftedAfterAnimation = 0;
+let positionToBeTopShift = 0;
+let positionToBeTopShiftedAfterAnimation = 0;
+let counter = 25;
 
 function nextSlideExecution() {
   animatorContainer.innerHTML = "";
 
-  map.forEach((item) => {
-    const span = document.createElement("span");
-    span.className =
-      item.value == 1 ? "code-snippet code-snippet__add" : "code-snippet";
-    span.textContent = item.key;
+  calculatedDiff.forEach((item) => {
+    const snippets = item[1].split("<~LB~>");
+    for (let i = 0; i < snippets.length; i++) {
+      const span = document.createElement("span");
+      span.className =
+        item["0"] == 1 ? "code-snippet code-snippet__add" : "code-snippet";
+      span.innerHTML = snippets[i].replaceAll(" ", "&nbsp;");
 
-    span.style.left = `${positionToBeLeftShift}px`;
+      span.style.left = `${positionToBeLeftShift}px`;
+      span.style.top = `${positionToBeTopShift}px`;
 
-    animatorContainer.appendChild(span);
+      animatorContainer.appendChild(span);
 
-    //to determine the current position after animation
-    const currentSnippetWidth = span.getBoundingClientRect().width;
-    //after redering if the element needs to be removed then turn the opacity 0 and turn display to none
-    if (item.value == -1) {
-      setTimeout(() => {
-        span.style.opacity = 0;
-      }, 500);
-      setTimeout(() => {
-        span.classList.add("code-snippet__removal");
-      }, 2100);
-    } else if (item.value == 1) {
-      span.style.display = "none";
-      const currPositionToBeLeftShifted = positionToBeShiftedAfterAnimation;
-      span.style.left = `${currPositionToBeLeftShifted}px`;
-      setTimeout(() => {
-        span.classList.remove("code-snippet__add");
-        span.style.opacity = 0;
-        span.style.display = "inline-block";
-      }, 2500);
-      setTimeout(() => {
-        span.style.opacity = 1;
-      }, 2600);
-      positionToBeShiftedAfterAnimation += currentSnippetWidth;
-    } else {
-      const currPositionToBeLeftShifted = positionToBeShiftedAfterAnimation;
-      setTimeout(() => {
+      const currentSnippetWidth = span.getBoundingClientRect().width;
+
+      if (item["0"] == -1) {
+        setTimeout(() => {
+          span.style.opacity = 0;
+        }, 500);
+        setTimeout(() => {
+          span.classList.add("code-snippet__removal");
+        }, 2100);
+      } else if (item["0"] == 1) {
+        span.style.display = "none";
+        const currPositionToBeLeftShifted =
+          positionToBeLeftShiftedAfterAnimation;
+        const currPositionToBeTopShifted = positionToBeTopShiftedAfterAnimation;
         span.style.left = `${currPositionToBeLeftShifted}px`;
-      }, 2600);
-      positionToBeShiftedAfterAnimation += currentSnippetWidth;
-    }
+        span.style.top = `${currPositionToBeTopShifted}px`;
+        setTimeout(() => {
+          span.classList.remove("code-snippet__add");
+          span.style.opacity = 0;
+          span.style.display = "inline-block";
+        }, 2500);
+        setTimeout(() => {
+          span.style.opacity = 1;
+        }, 2600);
+        positionToBeLeftShiftedAfterAnimation += currentSnippetWidth;
+        if (i + 1 < snippets.length != 0) {
+          positionToBeTopShiftedAfterAnimation += counter;
+          positionToBeLeftShiftedAfterAnimation = 0;
+        }
+      } else {
+        const currPositionToBeLeftShifted =
+          positionToBeLeftShiftedAfterAnimation;
+        const currPositionToBeTopShifted = positionToBeTopShiftedAfterAnimation;
+        setTimeout(() => {
+          span.style.left = `${currPositionToBeLeftShifted}px`;
+          span.style.top = `${currPositionToBeTopShifted}px`;
+        }, 2600);
+        positionToBeLeftShiftedAfterAnimation += currentSnippetWidth;
+        if (i + 1 < snippets.length != 0) {
+          positionToBeTopShiftedAfterAnimation += counter;
+          positionToBeLeftShiftedAfterAnimation = 0;
+        }
+      }
 
-    if (item.value != 1) {
-      positionToBeLeftShift += currentSnippetWidth;
+      if (item["0"] != 1) {
+        positionToBeLeftShift += currentSnippetWidth;
+        if (i + 1 < snippets.length != 0) {
+          positionToBeTopShift += counter;
+          positionToBeLeftShift = 0;
+        }
+      }
     }
   });
 }
